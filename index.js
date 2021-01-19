7174243b0c193a6cb7ea45d6ca6febd8cb3806a9
bostonvictormartinez <57546685+bostonvictormartinez@users.noreply.github.com>
2021-01-18 21:52:56 -0500

    using babel es6 backward compatible

---
+++ index.js
@@ -0,0 +1,10 @@
+es6 is backward compatible to es5 which is most code at this time.
+es6 couldnt remove all bad parts. javascript to ensure packagers would always work in context of es6
+when start writing es6 can use JS we know.
+
+
+let helloWorld='hello world';
+console.log(`${helloWorld} this is code.`);
+import {square, diag} from './lib.js';
+var sqrt=square(2);
+console.log(sqrt);
---
+++ index.mjs
@@ -0,0 +1,5 @@
+let helloWorld='hello world';
+console.log(`${helloWorld} this is code.`);
+import {square, diag} from './lib.js';
+var sqrt=square(2);
+console.log(sqrt);
